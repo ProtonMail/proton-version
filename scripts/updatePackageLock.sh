aa91607ff7597645b2079df191751e4ae24ac200
dhoko <dhoko@pm.me>
2020-02-06 11:08:36 +0100

    manage links and version

--- scripts/updatePackageLock.sh
+++ scripts/updatePackageLock.sh
@@ -14,7 +14,6 @@ if [[ "$CHANGED" = *"package-lock.json"* ]]; then
     # Commit only on default branch, we don't want it inside another branch -> prevent conflicts and default branch is the only source of truth
     if [[ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]]; then
         git add package-lock.json && git commit -m "Upgrade dependencies" || echo "Nope";
-        git push origin "$DEFAULT_BRANCH" || echo "Nope";
     else
         git checkout package-lock.json
     fi;
